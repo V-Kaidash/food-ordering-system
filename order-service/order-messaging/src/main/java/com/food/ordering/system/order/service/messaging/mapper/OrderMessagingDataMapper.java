905bddc63ad8764b99db0164d121881a7d26b9ee
V-Kaidash <dev.kaidash@gmail.com>
2025-06-06 16:17:03 +0200

    implemented message consumers.

--- order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/mapper/OrderMessagingDataMapper.java
+++ order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/mapper/OrderMessagingDataMapper.java
@@ -1,10 +1,16 @@
 package com.food.ordering.system.order.service.messaging.mapper;
 
+import com.food.ordering.system.domain.valueobject.OrderApprovalStatus;
+import com.food.ordering.system.domain.valueobject.PaymentStatus;
 import com.food.ordering.system.kafka.order.avro.model.PaymentOrderStatus;
 import com.food.ordering.system.kafka.order.avro.model.PaymentRequestAvroModel;
+import com.food.ordering.system.kafka.order.avro.model.PaymentResponseAvroModel;
 import com.food.ordering.system.kafka.order.avro.model.Product;
 import com.food.ordering.system.kafka.order.avro.model.RestaurantApprovalRequestAvroModel;
+import com.food.ordering.system.kafka.order.avro.model.RestaurantApprovalResponseAvroModel;
 import com.food.ordering.system.kafka.order.avro.model.RestaurantOrderStatus;
+import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
+import com.food.ordering.system.order.service.domain.dto.message.RestaurantApprovalResponse;
 import com.food.ordering.system.order.service.domain.entity.Order;
 import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
 import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
@@ -59,6 +65,32 @@ public class OrderMessagingDataMapper {
         .setPrice(order.getPrice().getAmount())
         .setCreatedAt(orderPaidEvent.getCreatedAt().toInstant())
         .setRestaurantOrderStatus(RestaurantOrderStatus.PAID)
+        .build();
+  }
+
+  public PaymentResponse paymentResponseAvroModelToPaymentResponse(PaymentResponseAvroModel paymentResponseAvroModel) {
+    return PaymentResponse.builder()
+        .id(paymentResponseAvroModel.getId().toString())
+        .sagaId(paymentResponseAvroModel.getSagaId().toString())
+        .paymentId(paymentResponseAvroModel.getPaymentId().toString())
+        .customerId(paymentResponseAvroModel.getCustomerId().toString())
+        .orderId(paymentResponseAvroModel.getOrderId().toString())
+        .price(paymentResponseAvroModel.getPrice())
+        .createdAt(paymentResponseAvroModel.getCreatedAt())
+        .paymentStatus(PaymentStatus.valueOf(paymentResponseAvroModel.getPaymentStatus().name()))
+        .failureMessages(paymentResponseAvroModel.getFailureMessages())
+        .build();
+  }
+
+  public RestaurantApprovalResponse approvalResponseAvroModelToRestaurantApprovalResponse(RestaurantApprovalResponseAvroModel restaurantApprovalResponseAvroModel) {
+    return RestaurantApprovalResponse.builder()
+        .id(restaurantApprovalResponseAvroModel.getId().toString())
+        .sagaId(restaurantApprovalResponseAvroModel.getSagaId().toString())
+        .restaurantId(restaurantApprovalResponseAvroModel.getRestaurantId().toString())
+        .orderId(restaurantApprovalResponseAvroModel.getOrderId().toString())
+        .createdAt(restaurantApprovalResponseAvroModel.getCreatedAt())
+        .orderApprovalStatus(OrderApprovalStatus.valueOf(restaurantApprovalResponseAvroModel.getOrderApprovalStatus().name()))
+        .failureMessages(restaurantApprovalResponseAvroModel.getFailureMessages())
         .build();
   }
 }
